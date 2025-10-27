beb16477d4caec94a93b9834d11b33b8fb8de2d5
upadhyan <nakulupadhya1@gmail.com>
2025-10-27 21:23:52 +0000

    Deploying to gh-pages from @ upadhyan/upadhyan.github.io@990e829066a91015c69d7e0da2184ff6b6adc3cd 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -198,16 +198,8 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-our-work-empowering-decision-trees-via-shape-function-branching-has-been-accepted-to-appear-at-neurips-2025-confetti-ball-read-more-here",
+          title: 'Our work “Empowering Decision Trees via Shape Function Branching” has been accepted to...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
