2b9fcf9ec6bb2dca43349b1dec145a4588b00089
upadhyan <nakulupadhya1@gmail.com>
2025-10-28 16:53:12 +0000

    Deploying to gh-pages from @ upadhyan/upadhyan.github.io@25a6f28f5c6fff79e73007f7732b9d726ad6da6e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,13 +30,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/repositories/";
           },
-        },{id: "nav-blog",
-          title: "blog",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/blog/";
-          },
         },{id: "nav-reading-list",
           title: "reading list",
           description: "",
